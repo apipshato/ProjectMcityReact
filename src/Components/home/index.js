d4235311d2d604e1988631b66ac98c0f289025f8
apipshato <afifshatos@gmail.com>
2019-02-07 15:07:45 +0700

    first commit

---
+++ src/Components/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Featured from './featured';
+
+
+const home = () => {
+    return (
+        <div className="bck_blue">
+            <Featured/>
+        </div>
+    );
+};
+
+export default home;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Hoc/Layout';
+import { Switch, Route } from 'react-router-dom';
+
+const Routes = (props) => {
+	return (
+		<Layout>
+			<Switch>second line</Switch>
+		</Layout>
+	);
+};
+
+export default Routes;
